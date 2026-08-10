d9b9c743b82683e44716b2c2b60fbd8989e0b722
Fermunizs <munizsilvestrefernanda@gmail.com>
2026-08-09 21:12:09 -0300

    Foi criado arquivo para estudo de declaração de variáveis em java

---
+++ conceitos/src/fundamentos/Variaveis.java
@@ -0,0 +1,15 @@
+package fundamentos;
+
+public class Variaveis {
+    public static void main(String[] args) {
+        String nome = "Fernanda";
+        int idade = 18;
+        double altura = 1.70;
+        boolean estudante = true;
+
+        System.out.println(nome);
+        System.out.println(idade);
+        System.out.println(altura);
+        System.out.println(estudante);
+    }
+}
