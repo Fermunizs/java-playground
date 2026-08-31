a3c2d247df1998106273b5462b0e40c26092492a
Fermunizs <munizsilvestrefernanda@gmail.com>
2026-08-31 20:25:20 -0300

    Foi criada a classe Despesa, foram definidos os atributos, configurado o construtor e criados os GETTERS E SETTERS

--- poo/Fintech/model/Despesa.java
+++ poo/Fintech/model/Despesa.java
@@ -1,11 +1,33 @@
 package model;
 
 public class Despesa {
+    private String categoria;
     private String descricao;
     private double valor;
+    private String data;
+    private String formaPagamento;
 
     public Despesa(String descricao, double valor) {
+        this.categoria = categoria;
         this.descricao = descricao;
         this.valor = valor;
+        this.data = data;
+        this.formaPagamento = formaPagamento;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public void setDescricao(String novaDescricao) {
+        this.descricao = novaDescricao;
+    }
+
+    public String getDespesa() {
+        return categoria + descricao + data + formaPagamento;
     }
 }
