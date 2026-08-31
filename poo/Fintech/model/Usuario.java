326b78ee306b7434d27b7055a39674f078422b18
Fermunizs <munizsilvestrefernanda@gmail.com>
2026-08-31 19:49:20 -0300

    Foram configurados os GETTERS e SETTERS

--- poo/Fintech/model/Usuario.java
+++ poo/Fintech/model/Usuario.java
@@ -4,17 +4,30 @@ public class Usuario{
     private String nome;
     private String email;
     private Integer idade;
-    private Double salario;
+    private Double saldo;
     private String cpf;
     private String senha;
 
-    public void Usuario(String nome, String email, Integer idade, Double salario, String cpf, String senha){
+    public void Usuario(String nome, String email, Integer idade, Double saldo, String cpf, String senha){
         this.nome = nome;
         this.email = email;
         this.idade = idade;
-        this.salario = salario;
+        this.saldo = saldo;
         this.cpf = cpf;
         this.senha = senha;
+    }
+
+    //GETTERS E SETTERS
+
+    public String getSaldo(){
+        return saldo.toString();
+    }
+    public String getNome(){
+        return nome;
+    }
+    public String getEmail(String novoEmail){
+        this.email = novoEmail;
+        return email;
     }
 
 }
